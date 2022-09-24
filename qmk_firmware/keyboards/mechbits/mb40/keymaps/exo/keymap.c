0daeaff66d6f8a2b6170a2b23cb8647ea4b72366
exo-uk <exo@techrebels.tv>
2022-09-24 12:29:43 +0100

    Edit _LAYER

--- qmk_firmware/keyboards/mechbits/mb40/keymaps/exo/keymap.c
+++ qmk_firmware/keyboards/mechbits/mb40/keymaps/exo/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       │Ctrl │ OS  │ Alt │ \│  │Lower│   Space   │Raise│  ←  │  ↓  │  ↑  │  →  │
       └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [LAYER_0] = LAYOUT_planck(
+    [_LAYER0] = LAYOUT_planck(
       KC_ESC,             KC_Q,     KC_W,     KC_E,     KC_R,  KC_T,   KC_Y,   KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
       KC_TAB,             KC_A,     KC_S,     KC_D,     KC_F,  KC_G,   KC_H,   KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
       TD(TD_SHIFT_CAPS),  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,  KC_N,   KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       │     │     │AltGr│RGBTg│Lower│           │Raise│Home │PgDn │PgUp │ End │
       └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [LAYER_1] = LAYOUT_planck(
+    [_LAYER1] = LAYOUT_planck(
       KC_GRV,   KC_1,   KC_2,     KC_3,     KC_4,    KC_5,    KC_6,    KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
       KC_NO,    KC_F1,  KC_F2,    KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_NUHS,
       KC_NO,    KC_F7,  KC_F8,    KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_NO,    RGB_VAD,  RGB_VAI,  KC_NO,    KC_NO,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       │Power│Sleep│     │Mute │Lower│    Play   │Raise│Prev │Vol- │Vol+ │Next │
       └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [LAYER_2] = LAYOUT_planck(
+    [_LAYER2] = LAYOUT_planck(
       KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_CALC,
       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  KC_PIPE,
       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       │MwLt │MwDn │MwRt │ M5  │Lower│   Space   │Raise│  0  │  ,  │  .  │Enter│
       └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [LAYER_3] = LAYOUT_planck(
+    [_LAYER3] = LAYOUT_planck(
       KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_PGUP,  KC_PSCR,  KC_UP,    KC_BSPC,  KC_PSLS,  KC_P7,  KC_P8,    KC_P9,    KC_PMNS,
       KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PAST,  KC_P4,  KC_P5,    KC_P6,    KC_PPLS,
       KC_BTN3,  KC_WH_U,  KC_BTN4,  KC_INS,   KC_PAUS,  KC_DEL,   KC_HOME,  KC_END,   KC_P1,  KC_P2,    KC_P3,    KC_EQL,
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       │     │     │     │     │Lower│           │Raise│     │AltGr│Debug│Reset│ 
       └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
     */
-    [LAYER_4] = LAYOUT_planck(
+    [_LAYER4] = LAYOUT_planck(
       KC_GRV,   KC_NO,  KC_E,   KC_4,   KC_NO,  KC_NO,  KC_NO,  KC_U,      KC_I,     KC_O,     KC_NO,    KC_NO,
       KC_NO,    KC_A,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RGB_MOD,   RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,
       KC_LSFT,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  RGB_RMOD,  RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,
