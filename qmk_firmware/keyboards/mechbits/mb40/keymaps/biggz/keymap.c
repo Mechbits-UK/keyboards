57d999adb97024a80025eb8c426dff8d61eb42a1
Biggz <biggler@gmail.com>
2022-09-24 15:57:02 +0100

    Updated Biggz keymap

--- qmk_firmware/keyboards/mechbits/mb40/keymaps/biggz/keymap.c
+++ qmk_firmware/keyboards/mechbits/mb40/keymaps/biggz/keymap.c
@@ -1,10 +1,12 @@
 #include QMK_KEYBOARD_H
 
+#define ENABLE_RGB_LAYERS false;
+
 enum my_layers {
-    _LAYER0,
-    _LAYER1,
-    _LAYER2,
-    _LAYER3,
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
 };
 
 // Tap Dance definitions
@@ -20,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
      * │Sft│ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │Ent│
      * ├───┼───┼───┼───┼───┼───┴───┼───┼───┼───┼───┼───┤
-     * |Ctl|Win│Alt│GUI│ < │       │ > │ ← │ ↓ │ ↑ │ → │
+     * |Ctl|Win│Alt│ \ │ < │       │ > │ ← │ ↓ │ ↑ │ → │
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
-    [_LAYER0] = LAYOUT_planck(
-        RGB_TOG,  KC_Q,    RGB_MODE_PLAIN,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        RGB_HUI,  RGB_HUD,    RGB_MODE_BREATHE,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        RGB_SAI, RGB_SAD,    RGB_MODE_KNIGHT,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-        RGB_VAI,  RGB_VAD, RGB_MODE_XMAS, RGB_TOG, MO(1),       KC_SPACE,       MO(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    [_QWERTY] = LAYOUT_planck(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+        KC_LCTL,  KC_LGUI, KC_LALT, KC_NUBS, MO(_LOWER), KC_SPACE,  MO(_RAISE),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
     /* Lower
     * ,-----------------------------------------------------------------------------------.
@@ -40,11 +42,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER1] = LAYOUT_planck(
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-        _______, _______, _______, _______, _______,       _______,      MO(3), _______,    KC_MNXT,    KC_VOLD, KC_VOLU
+    [_LOWER] = LAYOUT_planck(
+        KC_TILD, KC_EXLM, KC_DQUO,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, _______,
+        KC_DEL,  _______,   _______,   _______,   _______,   _______,   _______,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_TILDE,
+        _______, _______,   _______,   _______,   _______,  _______,  _______,  _______, KC_BTN1, KC_BTN2, KC_BTN3,  _______,
+        _______, _______, _______, _______, _______,       _______,      MO(_ADJUST), KC_MS_LEFT,    KC_MS_DOWN,    KC_MS_UP, KC_MS_RIGHT
     ),
     /* Raise
     * ,-----------------------------------------------------------------------------------.
@@ -57,11 +59,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER2] = LAYOUT_planck(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, MO(3),         _______,    _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    [_RAISE] = LAYOUT_planck(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_HASH,
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, MO(_ADJUST),         _______,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),    
     /* Combined
     * ,-----------------------------------------------------------------------------------.
@@ -74,24 +76,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      |      |      |      |      |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER3] = LAYOUT_planck(
+    [_ADJUST] = LAYOUT_planck(
+        RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, QK_REBOOT,
+        _______, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT
     ),
 };
+
+#ifdef ENABLE_RGB_LAYERS
 // LED Layers
-const rgblight_segment_t PROGMEM LED_layer0[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_BLUE});
-const rgblight_segment_t PROGMEM LED_layer1[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_GREEN});
-const rgblight_segment_t PROGMEM LED_layer2[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_YELLOW});
-const rgblight_segment_t PROGMEM LED_layer3[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_ORANGE});
+const rgblight_segment_t PROGMEM LED_QWERTY[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_ORANGE});
+const rgblight_segment_t PROGMEM LED_LOWER[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_BLUE});
+const rgblight_segment_t PROGMEM LED_RAISE[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_GREEN});
+const rgblight_segment_t PROGMEM LED_ADJUST[] = RGBLIGHT_LAYER_SEGMENTS( {0, 6, HSV_RED});
 // Define array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-        LED_layer0,
-        LED_layer1, // Overrides Layer 0
-        LED_layer2, // Overrides Layer 1
-        LED_layer3  // Overrides Layer 2
+        LED_QWERTY,
+        LED_LOWER, // Overrides Layer 0
+        LED_RAISE, // Overrides Layer 1
+        LED_ADJUST  // Overrides Layer 2
 );
 // Enable the LED layers
 void keyboard_post_init_user(void) {
@@ -103,8 +107,9 @@ layer_state_t default_layer_state_set_user(layer_state_t state) {
     return state;
 }
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
-    rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
-    rgblight_set_layer_state(3, layer_state_cmp(state, _LAYER3));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _LOWER));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
     return state;
 }
+#endif
